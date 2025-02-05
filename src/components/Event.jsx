951f97a09fb2f906a221fed785452e1d36725a98
ATTADASAIPRAHARSHA <saipraharshaattada@gmail.com>
2025-02-06 00:27:51 +0530

    css fixes

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect,useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../contexts/authContext';
 import PleaseLogin from './PleaseLogin';
@@ -7,14 +7,14 @@ import EventsSearch from './EventsSearch';
 import EventCaard from './EventCaard';
 
 const Event = () => {
-  const { CurrentUser, IsLoggedIn ,Events,fetchEvents} = useAuth();
+  const { CurrentUser, IsLoggedIn, Events, fetchEvents } = useAuth();
   const [currentEvent, setcurrentEvent] = useState('ongoing');
   const [DisplayedEvents, setDisplayedEvents] = useState([]);
 
   useEffect(() => {
     fetchEvents()
   }, [])
-  
+
 
   useEffect(() => {
     const currentDate = new Date().getTime();
@@ -24,7 +24,7 @@ const Event = () => {
       setDisplayedEvents(Events.filter((e) => new Date(e.start).getTime() <= currentDate && new Date(e.end).getTime() >= currentDate));
     } else if (currentEvent === 'upcoming') {
       setDisplayedEvents(Events.filter((e) => new Date(e.start).getTime() > currentDate));
-    } 
+    }
   }, [currentEvent, Events]);
 
   const handlecompleted = () => setcurrentEvent('completed');
@@ -35,36 +35,41 @@ const Event = () => {
       {IsLoggedIn ? (
         <>
           <EventBack />
-          <div className="blater options flex justify-center m-10">
-            <button
-              onClick={()=>handlecompleted()}
-              className={`${currentEvent === 'completed' ? 'bg-gray-300' : 'bglightBlue'} transition-all duration-950 border-2 p-2 text-xl border-black rounded-l-lg`}
-            >
-              Completed
-            </button>
+          <div className='flex h-[100vh]'>
+            <div className="blater options flex flex-col w-[20%] justify-center m-10 h-20">
+              <button
+                onClick={() => handlecompleted()}
+                className={`${currentEvent === 'completed' ? 'bg-gray-300' : 'bglightBlue'} transition-all duration-950 border-2 p-2 text-xl border-black rounded-l-lg`}
+              >
+                Completed
+              </button>
 
-            <button
-              onClick={handleongoing}
-              className={`${currentEvent === 'ongoing' ? 'bg-gray-300' : 'bglightBlue'} transition-all duration-950 border-2 p-2 text-xl border-black`}
-            >
-              Ongoing
-            </button>
+              <button
+                onClick={handleongoing}
+                className={`${currentEvent === 'ongoing' ? 'bg-gray-300' : 'bglightBlue'} transition-all duration-950 border-2 p-2 text-xl border-black`}
+              >
+                Ongoing
+              </button>
 
-            <button
-              onClick={handleupcoming}
-              className={`${currentEvent === 'upcoming' ? 'bg-gray-300' : 'bglightBlue'} transition-all duration-950 border-2 p-2 text-xl border-black rounded-r-lg`}
-            >
-              Upcoming
-            </button>
+              <button
+                onClick={handleupcoming}
+                className={`${currentEvent === 'upcoming' ? 'bg-gray-300' : 'bglightBlue'} transition-all duration-950 border-2 p-2 text-xl border-black rounded-r-lg`}
+              >
+                Upcoming
+              </button>
 
-          </div>
-          <EventsSearch />
-          <div className="events">
-            {DisplayedEvents.length > 0 ?
-              DisplayedEvents.map((eve, index) => {
-                return <EventCaard  key={index} events={eve} />;
-              }) : <div className='flex justify-center items-center m-4 p-4'>Sorry No Events</div>
-            }
+            </div>
+            <div className='overflow-scroll w-[80%] my-5'>
+              <EventsSearch />
+              <div className="events ">
+                {DisplayedEvents.length > 0 ?
+                  DisplayedEvents.map((eve, index) => {
+                    return <EventCaard key={index} events={eve} />;
+                  }) : <div className='flex justify-center items-center m-4 p-4'>Sorry No Events</div>
+                }
+              </div>
+            </div>
+
           </div>
         </>
       ) : (
