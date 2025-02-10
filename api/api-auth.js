b9e8395e93d6f4876fb16e3a8578939de1092c4d
ATTADA SAI PRAHARSHA <147126924+ATTADASAIPRAHARSHA@users.noreply.github.com>
2025-02-10 19:27:05 +0530

    debugging email

--- api/api-auth.js
+++ api/api-auth.js
@@ -41,6 +41,7 @@ export default async function handler(req, res) {
       console.log('hi')
       const decodedToken = await admin.auth().verifyIdToken(token);
       const email = decodedToken.email; 
+      console.log(email)
       const { data: user, error } = await supabase
         .from('users')
         .select('*')
