655f8db23c771d4c9c48cc4dcdc3146711993abb
ATTADASAIPRAHARSHA <saipraharshaattada@gmail.com>
2025-02-14 14:08:48 +0530

    routes checking

--- api/index.js
+++ api/index.js
@@ -11,8 +11,8 @@ app.use(express.json());
 // Export the app correctly for Vercel serverless functions
 
 
-app.use('/',loginRoutes);
-app.use('/',googleRoutes);
+app.use('/api',loginRoutes);
+app.use('/api',googleRoutes);
 
 // app.listen(3000,() => console.log('server listneing on port 3000'))
 export default app;
