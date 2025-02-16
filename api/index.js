59bfb0e8a8664549937fec3076e8f9b930d2fd78
ATTADASAIPRAHARSHA <saipraharshaattada@gmail.com>
2025-02-17 03:04:15 +0530

    into the supabase

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import loginRoutes from './routes/login.js';
 import googleRoutes from './routes/google.js';
 import userData from './routes/getuser.js'
+import eventsData from './routes/events-get.js'
+import eventsPost from './routes/events-post.js'
 const app = express();
 app.use(express.json());
 
@@ -14,6 +16,8 @@ app.use(express.json());
 app.use('/api',loginRoutes);
 app.use('/api',googleRoutes);
 app.use('/api',userData);
+app.use('/api',eventsData);
+app.use('/api',eventsPost);
 
 // app.listen(3000,() => console.log('server listneing on port 3000'))
 export default app;
