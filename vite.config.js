4a0b399fd1eb902a237a510a35a0e67706de8006
ATTADASAIPRAHARSHA <saipraharshaattada@gmail.com>
2025-02-20 01:55:50 +0530

    checking log out

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,15 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  const env = loadEnv(mode, process.cwd(), '');
 
   return {
     plugins: [react()],
     define: {
-      'process.env': process.env, // Optional for debugging
+      'import.meta.env': env,
+    },
+    build: {
+      target: 'esnext', // Enable top-level await support
     },
   };
 });
